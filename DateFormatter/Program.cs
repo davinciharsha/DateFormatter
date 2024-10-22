3c2fc635a9b6d7e5f6d1d71141405284277552d3
davinciharsha <davinciharsha@gmail.com>
2024-10-22 18:34:18 +0530

    fixed minor issues and code refactoring

--- DateFormatter/Program.cs
+++ DateFormatter/Program.cs
@@ -8,44 +8,55 @@ namespace DateFormatter
     {
         static void Main(string[] args)
         {
+
             // accept date value from user.
             // have a separate method which accepts date, in which seggregate date, month and year
             // have an array of days in month to check, if by adding 1 to the received date would cause an overflow.
             // if yes, then increment the month as well. else, dont
             // also check if incrementing the month is not greater than 12, if it is greater, then increment the year as well.
             // return the value.
-
             Console.WriteLine("Date formatter program.");
             Console.WriteLine("Enter date in dd/mm/yyyy format.");
+            string datetime = Console.ReadLine();
+
+            Console.WriteLine("Enter number of days to be added to the given date.");
+            string daysToAdd = Console.ReadLine();
+
+            string updatedDate = DateOperations.InitialValidation(datetime, daysToAdd);
+            Console.WriteLine($"Updated date = {updatedDate}");
+
+        }
+
+    }
+
+    public class DateOperations
+    {
+        public static string InitialValidation(string datetime, string daysToAdd)
+        {
 
             DateTime dateTime;
 
             try
             {
-                dateTime = DateTime.Parse(Console.ReadLine());
+                dateTime = DateTime.Parse(datetime);
 
             }
             catch (FormatException ex)
             {
-                Console.WriteLine($"Invalid date format received {ex.Message}.");
-                throw;
+                throw new FormatException($"Invalid date format received {ex.Message}.");
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Unexpected error occurred {ex.Message}.");
-                throw;
+                throw new Exception($"Unexpected error occurred {ex.Message}.");
             }
 
-            Console.WriteLine("Enter number of days to be added to the given date.");
-            string daysToAdd = Console.ReadLine();
 
             if (!int.TryParse(daysToAdd, out int addDays))
             {
                 throw new InvalidCastException($"Invalid number of days entered. {daysToAdd}");
             }
 
-            string updatedDate = AddDays(dateTime, addDays);
-            Console.WriteLine($"Updated date = {updatedDate}");
+            return AddDays(dateTime, addDays);
         }
 
         // keeping public so that I can test    
@@ -60,7 +71,7 @@ namespace DateFormatter
             int year = date.Year;
 
             // identify if the received year is leap year and update days in 1st index position of array to 29
-            if(IsLeapYear(year))
+            if (IsLeapYear(year))
             {
                 daysInMonths[1] = 29;
             }
@@ -69,23 +80,19 @@ namespace DateFormatter
 
             // checking if the days crossed the maximum days in month
             // 28th in march + 5 days = 33, but maximum days in march are 31. so 33-31 = 2 of april
-            if (day > daysInMonths[month - 1])
+            while (day > daysInMonths[month - 1])
             {
                 day = day - daysInMonths[month - 1];
-                month = month++;
+                month = ++month;
 
                 if (month > 12)
                 {
                     month = 1;
-                    year = year++;
+                    year = ++year;
                 }
+            }
 
-                return new DateTime(year, month, day).ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
-            }
-            else
-            {
-                return new DateTime(year, month, day).ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
-            }
+            return new DateTime(year, month, day).ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
 
         }
 
@@ -94,6 +101,7 @@ namespace DateFormatter
         {
             return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
         }
+
     }
 }
 
